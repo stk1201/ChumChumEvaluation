f3fc429915ed5a182b11db3c07dee370d7f534f4
stk1201 <satokichi121@gmail.com>
2024-02-14 14:31:06 +0900

    Merge remote-tracking branch 'origin/main'

--- ChumChumEvaluation/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0688hf/chumchumevaluation/Uploading.java
+++ ChumChumEvaluation/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0688hf/chumchumevaluation/Uploading.java
@@ -2,6 +2,7 @@ package jp.ac.ritsumei.ise.phy.exp2.is0688hf.chumchumevaluation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.VideoView;
@@ -19,7 +20,15 @@ public class Uploading extends AppCompatActivity {
    VideoView user_dance;
 
     public void onUploadButtonTapped(View View){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("video/*");
+        startActivityForResult(intent, 10);
 
+    }
 
+    @Override
+    protected void onActivityResult(){
+        super.onActivityResult(request);
     }
 }
