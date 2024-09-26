a723fa40b7bf13b0d3c29f78a03c75068f969e89
stk1201 <satokichi121@gmail.com>
2024-09-26 09:37:58 +0900

    ローディング画面の自動遷移

--- app/src/main/java/jp/ac/ritsumei/ise/phy/exp2is0667fe/danceevaluation/LoadingActivity.java
+++ app/src/main/java/jp/ac/ritsumei/ise/phy/exp2is0667fe/danceevaluation/LoadingActivity.java
@@ -2,6 +2,7 @@ package jp.ac.ritsumei.ise.phy.exp2is0667fe.danceevaluation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoadingActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+
+        Intent intent = new Intent(this, Result1Activity.class);
+        startActivity(intent);
     }
 }
